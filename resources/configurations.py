1cda5c941d4e1ab7e3990a2568399a5df22c137b
gorih <dbulaniy@niuz.co>
2018-07-02 11:19:56 +0300

    changed configurations

--- resources/configurations.py
+++ resources/configurations.py
@@ -1,8 +1,8 @@
-INDEX_FILE_PATH = "/home/gorih/PycharmProjects/faiss-web-service/resources/index"
+INDEX_FILE_PATH = "/fws/faiss-web-service/resources/index"
 
-IDS_MAP_FILE_PATH = "/home/gorih/PycharmProjects/faiss-web-service/resources/ids_vectors.p"
+IDS_MAP_FILE_PATH = "/fws/faiss-web-service/resources/ids_vectors.p"
 
-WORDS_VECTORS_FILE_PATH = "/home/gorih/Documents/fastText/wiki.en/wiki.en.vec"
+WORDS_VECTORS_FILE_PATH = "/fws/fast_text_models/wiki_vec"
 
 LANGUAGE = "english"
 
@@ -14,14 +14,15 @@ REDIS_HOST = "localhost"
 
 REDIS_PORT = 6379
 
-REDIS_DB = 2
+REDIS_DB = 1
 
 INDEX_WRITES_FREQUENCY_SEC = 60
 
-APP_HOST = "localhost"
+APP_HOST = "146.185.132.183"
 
-APP_PORT = 5000
+APP_PORT = 5002
 
 LOAD_VOCABULARY = True
 
 PERSIST_BODIES = True
+
